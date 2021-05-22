f1c242e56e1ae19fdb6c773ca34bb52ec4d000c2
Helen Essang <lennyessang@gmail.com>
2021-05-22 16:32:09 -0400

    Initial commit

---
+++ main.js
@@ -0,0 +1,14 @@
+function convertFahrToCelsius (F){
+    const cal = (F - 32) * 5/9
+    const result = cal.toFixed(4);
+    if(typeof (F) !== "number") {
+        console.log(`${F} is not a valid number but a/an ${typeof(F)}`)
+      }
+        console.log(result)
+      
+}
+
+convertFahrToCelsius(0)
+convertFahrToCelsius("0")
+convertFahrToCelsius([1,2,3])
+convertFahrToCelsius({temp: 0})
